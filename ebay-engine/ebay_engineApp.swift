e2f7f6f180330b5376e2e60d43db8c9b31dec769
ColinZWang <wangzixi@usc.edu>
2023-11-14 14:47:22 -0800

    Initial Commit

---
+++ ebay-engine/ebay_engineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebay_engineApp.swift
+//  ebay-engine
+//
+//  Created by Colin Wang on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ebay_engineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
