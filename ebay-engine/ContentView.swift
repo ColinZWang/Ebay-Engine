ff3e1a0ac591e62c1f3f1cf5cf5c5bd4911ee492
ColinZWang <wangzixi@usc.edu>
2023-11-27 21:16:47 -0800

    Photos Tab

--- ebay-engine/ContentView.swift
+++ ebay-engine/ContentView.swift
@@ -490,7 +490,7 @@ struct ItemDetailView: View {
             }
 
             // Photos Tab
-            Text("Photos Placeholder")
+            PhotosView(productTitle: productDetails?.Title ?? "")
                 .tabItem {
                     Label("Photos", systemImage: "photo")
                 }
@@ -625,6 +625,67 @@ struct InfoRow: View {
         }
     }
 }
+
+struct PhotosView: View {
+    @State private var imageLinks: [String] = []
+    @State private var isLoading = false
+    let productTitle: String
+
+    var body: some View {
+        ScrollView {
+            if isLoading {
+                ProgressView()
+            } else {
+                ForEach(imageLinks, id: \.self) { link in
+                    if let url = URL(string: link) {
+                        AsyncImage(url: url) { phase in
+                            switch phase {
+                            case .empty:
+                                ProgressView()
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            case .failure:
+                                Text("Couldn't load image")
+                            @unknown default:
+                                EmptyView()
+                            }
+                        }
+                        .frame(width: 300, height: 200) // or use another frame size
+                    }
+                }
+            }
+        }
+        .onAppear {
+            loadImages()
+        }
+    }
+
+    private func loadImages() {
+        guard let encodedTitle = productTitle.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: "http://localhost:8080/photos?productTitle=\(encodedTitle)") else {
+            print("Invalid URL")
+            return
+        }
+
+        isLoading = true
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, let imageLinks = try? JSONDecoder().decode([String].self, from: data) {
+                DispatchQueue.main.async {
+                    self.imageLinks = imageLinks
+                    isLoading = false
+                }
+            } else {
+                DispatchQueue.main.async {
+                    isLoading = false
+                    print("Error fetching photos: \(error?.localizedDescription ?? "Unknown error")")
+                }
+            }
+        }.resume()
+    }
+}
+
 
 
 
